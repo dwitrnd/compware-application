55bdae05521701fc10b6d8a58ef6ca14f7199574
domster4444 <kshitiz.shah@deerwalk.edu.np>
2023-05-22 03:06:38 +0545

    FEAT: add concurrently

--- client/src/screen/auth/SendPasswordResetEmail.js
+++ client/src/screen/auth/SendPasswordResetEmail.js
@@ -1,87 +1,56 @@
-import { Grid, TextField, Button, Box, Alert } from '@mui/material';
-import { useState } from 'react';
-import { useSendPasswordResetEmailMutation } from '../../redux/api/auth/userAuthApi';
+import { Grid, TextField, Button, Box, Alert } from "@mui/material";
+import { useState } from "react";
+import { useSendPasswordResetEmailMutation } from "../../redux/api/auth/userAuthApi";
 const SendPasswordResetEmail = () => {
-  const [sendPasswordResetEmail, { isLoading, isError, isSuccess }] =
-    useSendPasswordResetEmailMutation();
+  const [sendPasswordResetEmail, { isLoading, isError, isSuccess }] = useSendPasswordResetEmailMutation();
   const [error, setError] = useState({
     status: false,
-    msg: '',
-    type: '',
+    msg: "",
+    type: "",
   });
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
     const actualData = {
-      email: data.get('email'),
+      email: data.get("email"),
     };
     if (actualData.email) {
       console.log(actualData);
       const res = await sendPasswordResetEmail(actualData);
       console.log(res);
-      if (res.data.status === 'success') {
-        document.getElementById('password-reset-email-form').reset();
+      if (res.data.status === "success") {
+        document.getElementById("password-reset-email-form").reset();
         setError({
           status: true,
-          msg: 'Password Reset Email Sent. Check Your Email !!',
-          type: 'success',
+          msg: "Password Reset Email Sent. Check Your Email !!",
+          type: "success",
         });
       }
-      if (res.error.data.status === 'failed') {
+      if (res.error.data.status === "failed") {
         setError({
           status: true,
           msg: res.error.data.message,
-          type: 'error',
+          type: "error",
         });
       }
     } else {
       setError({
         status: true,
-        msg: 'Please Provide Valid Email',
-        type: 'error',
+        msg: "Please Provide Valid Email",
+        type: "error",
       });
     }
   };
   return (
     <>
-      <Grid container justifyContent="center">
-        <Grid item sm={6} xs={12}>
-          <h1>Reset Password</h1>
-          <Box
-            component="form"
-            noValidate
-            sx={{ mt: 1 }}
-            id="password-reset-email-form"
-            onSubmit={handleSubmit}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              name="email"
-              label="Email Address"
-            />
-            <Box textAlign="center">
-              <Button
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2, px: 5 }}
-              >
-                Send
-              </Button>
-            </Box>
-            {error.status ? (
-              <Alert severity={error.type}>{error.msg}</Alert>
-            ) : (
-              ''
-            )}
-          </Box>
-        </Grid>
-      </Grid>
+      <h1>Reset Password</h1>
+      <form id='password-reset-email-form' onSubmit={handleSubmit}>
+        <input required id='email' name='email' label='Email Address' />
+        <button type='submit'>Send</button>
+        {error.status ? <div>{error.msg}</div> : ""}
+      </form>
     </>
   );
 };
 
 export default SendPasswordResetEmail;
-
