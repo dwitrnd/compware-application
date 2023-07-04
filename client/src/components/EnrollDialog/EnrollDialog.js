87a256d8cc2d39193a683d1fb8c50387731d4d26
Tushar Luitel <tusharluitel88@gmail.com>
2023-07-04 16:23:21 +0545

    Chore: Updated entroll dialog box

--- client/src/components/EnrollDialog/EnrollDialog.js
+++ client/src/components/EnrollDialog/EnrollDialog.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import { Typography } from "@mui/material";
+import { FormGroup, IconButton, Typography } from "@mui/material";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import CloseIcon from "@mui/icons-material/Close";
+import { Link } from "react-router-dom";
 
 const EnrollDialog = () => {
   const [open, setOpen] = useState(false);
@@ -26,20 +28,84 @@ const EnrollDialog = () => {
       >
         Enroll
       </Button>
-      <Dialog open={open} onClose={handleClose}>
+      <Dialog open={open} onClose={handleClose} maxWidth="md">
         <form>
-          <DialogTitle>Register Now</DialogTitle>
+          <DialogTitle
+            display="flex"
+            justifyContent="space-between"
+            color="primary"
+          >
+            Register Now{" "}
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              sx={{
+                marginLeft: "auto",
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
           <DialogContent>
             <Typography variant="body1">Name</Typography>
-            <TextField variant="outlined" id="name" />
-            <Typography variant="body1">Email</Typography>
-            <TextField variant="outlined" id="email" />
-            <Typography variant="body1">Phone</Typography>
-            <TextField variant="outlined" id="phone" />
-            <Typography variant="body1">Course</Typography>
-            <TextField variant="outlined" id="course" />
-            <Typography variant="body1">Schedule</Typography>
-            <TextField variant="outlined" id="schedule" />
+            <TextField variant="outlined" id="name" sx={{ width: "100%" }} />
+            <Typography variant="body1" style={{ marginTop: "0.75rem" }}>
+              Email
+            </Typography>
+            <TextField variant="outlined" id="email" sx={{ width: "100%" }} />
+            <Typography variant="body1" style={{ marginTop: "0.75rem" }}>
+              Phone
+            </Typography>
+            <TextField variant="outlined" id="phone" sx={{ width: "100%" }} />
+            <Typography variant="body1" style={{ marginTop: "0.75rem" }}>
+              Course
+            </Typography>
+            <TextField variant="outlined" id="course" sx={{ width: "100%" }} />
+            <Typography variant="body1" style={{ marginTop: "0.75rem" }}>
+              Schedule
+            </Typography>
+            <TextField
+              variant="outlined"
+              id="schedule"
+              sx={{ width: "100%" }}
+            />
+            <FormGroup>
+              <div
+                style={{
+                  display: "flex",
+                  FlexDirection: "row",
+                }}
+              >
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      style={{ position: "relative", paddingLeft: "0.5rem" }}
+                    />
+                  }
+                />
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginTop: "1rem",
+                    marginBottom: "1rem",
+                  }}
+                >
+                  <span style={{ marginRight: "0.25rem" }}>I agree to</span>
+                  <Link to="/terms-and-condition" target="_blank">
+                    <Typography variant="body1" color="primary">
+                      Terms and Conditions
+                    </Typography>
+                  </Link>
+                </div>
+              </div>
+            </FormGroup>
+            <Button
+              variant="contained"
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              Register
+            </Button>
           </DialogContent>
         </form>
       </Dialog>
