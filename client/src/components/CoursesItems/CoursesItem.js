87e1a5be87536ebd99d6ed8faa77288e71318fde
Tushar Luitel <tusharluitel88@gmail.com>
2023-07-07 16:44:37 +0545

    Style: Changed layout of courses

--- client/src/components/CoursesItems/CoursesItem.js
+++ client/src/components/CoursesItems/CoursesItem.js
@@ -52,6 +52,46 @@ const CoursesItem = () => {
         </Grid>
         <CourseEnrollDialog courseName={course} />
       </Box>
+      <Box
+        sx={{
+          display: "inline-block",
+          height: "100%",
+          borderRadius: "0.75rem",
+          margin: "1.25rem",
+          padding: "1.75rem",
+          boxShadow: "6px 6px 12px 3px rgba(99, 99, 99, 0.20)",
+        }}
+      >
+        <Stack direction="row" spacing={2}>
+          <div>
+            <Stack direction="column">
+              <img
+                src={ExpressJs}
+                style={{ height: "10rem", width: "10rem" }}
+              />
+              <Typography variant="h6" color="primary">
+                {course}
+              </Typography>
+              <Typography variant="subtitle1">Duration:</Typography>
+              <Typography variant="subtitle1">Schedule:</Typography>
+            </Stack>
+          </div>
+          <div>
+            <Typography>
+              Bachelor's Degree from Ratna Rajyalaxmi Campus (Tribhuvan
+              University) in the faculty of humanities and social science, IT
+              Apprenticeship program from CTEVT (Deerwalk Institute of
+              Technology) and Level-3 Award in Education and Training (RQF) from
+              Highfield, Eager for MENA (Middle East and North Africa); Supreme
+              Committee for Delivery and Legacy, (FIFA World Cup Qatar 2022™) as
+              HSSE Trainer and Facilitator, and Sifal Secondary School as an
+              Admin Analyst. He is also working as a Sales and Marketing Manager
+              at Logispark
+            </Typography>
+          </div>
+        </Stack>
+        <CourseEnrollDialog courseName={course} />
+      </Box>
     </>
   );
 };
