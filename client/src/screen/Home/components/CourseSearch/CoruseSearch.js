1db6b1db78ef8ae5220bb8540b16360488a0de3f
domster4444 <kshitiz.shah@deerwalk.edu.np>
2023-08-09 12:46:31 +0545

    Feat(autosuggest): add redirect to course detail page on click of course search

--- client/src/screen/Home/components/CourseSearch/CoruseSearch.js
+++ client/src/screen/Home/components/CourseSearch/CoruseSearch.js
@@ -1,34 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
-
-const courseOptions = [
-  "ANDROID PROGRAMMING",
-  "INTRODUCTION TO MICROSOFT SQL SERVER",
-  "INTRODUCTION TO THE CRASH COURSE FOR SEE STUDENTS",
-  "AWS CLOUD PRACTITIONER",
-  "PROGRAMMING IN PYTHON",
-  "REACT NATIVE",
-  "CISCO CERTIFIED NETWORK ASSOCIATE (CCNA)",
-  "COMPLETE WEB DEVELOPER COURSE",
-  "CYBER SECURITY",
-  "DATA SCIENCE AND MACHINE LEARNING IN PYTHON",
-];
+import axios from "axios";
+import { constant } from "constants/contants";
+import { useNavigate } from "react-router-dom";
 
 const CourseSearch = () => {
+  const [allCourseName, setAllCourseName] = React.useState([]);
+  const [courseNameAndId, setCourseNameAndId] = React.useState([
+    {
+      courseName: "",
+      courseId: "",
+    },
+  ]);
+  // axios code to fetch all data get method from "/api/course"
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`${constant.base}/api/course`).then((res) => {
+      if (res) {
+        console.log("res.data============================================================================");
+        console.log(res.data);
+        const allCourseList = [];
+        res.data.msg.map((course) => {
+          allCourseList.push(course.courseName);
+          setCourseNameAndId((prev) => [
+            ...prev,
+            {
+              courseName: course.courseName,
+              courseId: course._id,
+            },
+          ]);
+
+          setAllCourseName(allCourseList);
+        });
+      }
+    });
+  }, []);
+
   return (
     <Autocomplete
-      id="autocomplete-search"
-      options={courseOptions}
+      id='autocomplete-search'
+      options={allCourseName}
       getOptionLabel={(option) => option}
-      renderInput={(params) => (
-        <TextField {...params} label="Search" variant="outlined" />
-      )}
+      renderInput={(params) => <TextField {...params} label='Search' variant='outlined' />}
       sx={{
         backgroundColor: "white",
         width: "50%",
         marginLeft: "25%",
         borderRadius: "50px",
+      }}
+      onInputChange={(event, value) => {
+        console.log(value);
+
+        // check what id is for the course name
+        courseNameAndId.map((course) => {
+          if (course.courseName === value) {
+            console.log(course.courseId);
+            navigate(`/course-detail/${course.courseId}`);
+          }
+        });
       }}
     />
   );
