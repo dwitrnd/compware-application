0567813e8150948647bdf4a85a1e0c48ff5555a5
Tushar Luitel <tusharluitel88@gmail.com>
2023-09-05 18:45:37 +0545

    Chore: Changed value at constants.js

--- client/src/services/UserApiService.js
+++ client/src/services/UserApiService.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import axios from "axios";
 export class UserApiService {
   static serverURL = "http://192.168.0.211";
-=======
-import axios from 'axios';
-export class UserApiService {
-  static serverURL = 'http://localhost:8080';
->>>>>>> final-tushar-2
   static getAllUsers() {
     // let dataURL = `${UserApiService.serverURL}/users`;
     // return axios.get(dataURL);
