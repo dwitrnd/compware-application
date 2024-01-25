b02d503a61086183d09d074eaba1ee0b961ac70d
pratishma09 <velv9tkiss3s@gmail.com>
2024-01-25 17:29:00 +0545

    Update(Ep 2):Changed content of Episode 2 event page

--- client/src/screen/Event/EventPageEpisode2.js
+++ client/src/screen/Event/EventPageEpisode2.js
@@ -120,14 +120,7 @@ const EventPageEpisode2 = () => {
         }}
       >
         <h3>
-          Building upon the success of Deerwalk Tech Dialogue Series | Episode I
-          : "Making Nepal the IT Hub by 2030 - Possibilities and Challenges", we
-          are excited to introduce Deerwalk Tech Dialogue Series | Episode II :
-          "Deconstructing the IT Curriculum-Industry Skills Mismatch - A
-          Comprehensive Review of Nepalese University Curricula and IT Needs."
-          In this upcoming episode, we will continue our discussion by examining
-          the key insights from episode 1 and working towards practical
-          solutions for bridging the gap between education and industry demands.
+        The Deerwalk Tech Dialogue Series EP - II, held on December 29, 2023, convened experts to discuss transforming Nepal into an IT hub by 2030. Delving into academia and industry intersections, concerns over the current curriculum's relevance and the urgent need for an overhaul were highlighted. Academic figures included Rohit Raj Pandey, Dr. Bhoj Raj Ghimire, Himalaya Kakshyapati, Shyam Sundar Khatiwada, and Kumar Lamichhane, while industry experts featured Shristi Kayastha, Santosh Koirala, Sweta Karn, Subash Devkota, and Chiranjibi Adhikari. Key discussions focused on bridging academia-industry gaps, addressing challenges like curriculum rigidity, soft skills deficiency, and the need for continuous updates. Emphasizing collaboration and practical skill development, the session aimed at retaining talent in Nepal's evolving IT sector, addressing issues from brain drain to the impact of salary boundaries on students' role preferences.
         </h3>
       </section>
       {/* host section */}
