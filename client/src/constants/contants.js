79bb2ce396ae36cfb19fd829650f22f4cd3721e7
pratishma09 <velv9tkiss3s@gmail.com>
2024-02-15 15:59:11 +0545

    feat(popup):Popup added to the homepage

--- client/src/constants/contants.js
+++ client/src/constants/contants.js
@@ -1,6 +1,6 @@
 export const constant = {
-  base: "https://api.deerwalktrainingcenter.com",
-  client: "https://deerwalktrainingcenter.com",
-  // base: "http://localhost:5001",
-  // client: "http://localhost:3000",
+  // base: "https://api.deerwalktrainingcenter.com",
+  // client: "https://deerwalktrainingcenter.com",
+  base: "http://localhost:5001",
+  client: "http://localhost:3000",
 };
