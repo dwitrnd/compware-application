3dfa29b69cf6b9772e21c841f360b772abde23e0
pratishma09 <velv9tkiss3s@gmail.com>
2024-08-09 16:38:45 +0545

    fix(client):dynamic client in homepage

--- client/src/screen/Home/components/Clients/Clients.js
+++ client/src/screen/Home/components/Clients/Clients.js
@@ -1,39 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import { constant } from "constants/contants";
 import Box from "@material-ui/core/Box";
 import axios from "axios";
-
-import Client1 from "../../../../assets/images/compware-clients/citizenbamk.jpg";
-import Client2 from "../../../../assets/images/compware-clients/crimsoncollege.jpg";
-import Client3 from "../../../../assets/images/compware-clients/Deraz.jpg";
-import Client4 from "../../../../assets/images/compware-clients/Everest-Bank.jpg";
-import Client5 from "../../../../assets/images/compware-clients/Global-IME-Bank.jpg";
-import Client6 from "../../../../assets/images/compware-clients/hamro-patro.jpg";
-import Client7 from "../../../../assets/images/compware-clients/handicap-international.jpg";
-import Client8 from "../../../../assets/images/compware-clients/himalaya-college.jpg";
-import Client9 from "../../../../assets/images/compware-clients/jyoti-bikash.jpg";
-import Client10 from "../../../../assets/images/compware-clients/KEC.jpg";
-import Client11 from "../../../../assets/images/compware-clients/khalti.jpg";
-import Client12 from "../../../../assets/images/compware-clients/NEA.jpg";
-import Client13 from "../../../../assets/images/compware-clients/Nepal-bank-Limited.jpg";
-import Client14 from "../../../../assets/images/compware-clients/nic.jpg";
-import Client15 from "../../../../assets/images/compware-clients/nmb-bank.jpg";
-import Client16 from "../../../../assets/images/compware-clients/NTc.jpg";
-import Client17 from "../../../../assets/images/compware-clients/prabhy-pay.jpg";
-import Client18 from "../../../../assets/images/compware-clients/Radisson.jpg";
-import Client19 from "../../../../assets/images/compware-clients/save-the-childern.jpg";
-import Client20 from "../../../../assets/images/compware-clients/shivapuri-school.jpg";
-import Client21 from "../../../../assets/images/compware-clients/siddharthaBank.jpg";
-import Client22 from "../../../../assets/images/compware-clients/vianet.jpg";
-import Client23 from "../../../../assets/images/compware-clients/worldlink.jpg";
-import Client24 from "../../../../assets/images/compware-clients/US Embassy.png";
-import Client25 from "../../../../assets/images/compware-clients/Shangri La development bank.png";
 import styled from "styled-components";
+import { constant } from "constants/contants";
 
 const ClientImage = styled.img`
-  /* filter: grayscale(100%); */
   transition: all 0.3s ease-in-out;
   &:hover {
     filter: grayscale(0%);
@@ -55,67 +27,34 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Testimonials(props) {
   const classes = useStyles();
+  const [clients, setClients] = useState([]);
+  const url = `${constant.base}/api/client`;
   useEffect(() => {
-    const url = `${constant.base}/api/client`;
-  });
+    const fetchClients = async () => {
+      try {
+        const response = await axios.get(url);
+        console.log(response);
+        setClients(response.data.msg);
+        // console.log(setClients);
+      } catch (error) {
+        console.error("Error fetching clients", error);
+      }
+    };
 
-  const content = {
-    logo1: Client1,
-    logo2: Client2,
-    logo3: Client3,
-    logo4: Client4,
-    logo5: Client5,
-    logo7: Client7,
-    logo8: Client8,
-    logo9: Client9,
-    logo10: Client10,
-    logo11: Client11,
-    logo12: Client12,
-    logo13: Client13,
-    logo14: Client14,
-    logo15: Client15,
-    logo16: Client16,
-    logo17: Client17,
-    logo18: Client18,
-    logo19: Client19,
-    logo20: Client20,
-    logo21: Client21,
-    logo22: Client22,
-    logo23: Client23,
-    logo24: Client6,
-    logo25: Client24,
-    logo26: Client25,
-    ...props.content,
-  };
+    fetchClients();
+  }, []);
 
   return (
     <section>
-      <Box pt={4} display="flex" flexWrap="wrap" justifyContent="space-between">
-        <ClientImage src={content["logo1"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo2"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo3"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo4"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo5"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo6"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo7"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo8"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo9"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo10"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo11"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo12"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo13"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo14"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo15"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo16"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo17"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo18"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo19"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo20"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo22"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo23"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo24"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo25"]} alt="" className={classes.logo} />
-        <ClientImage src={content["logo26"]} alt="" className={classes.logo} />
+      <Box pt={4} display="flex" flexWrap="wrap" justifyContent="center">
+        {clients.map((client, index) => (
+          <ClientImage
+            key={index}
+            src={`${constant.base}/storage/${client.Image}`}
+            alt={client.ImageName}
+            className={classes.logo}
+          />
+        ))}
       </Box>
     </section>
   );
